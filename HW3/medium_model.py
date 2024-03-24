9ac9bf1f7ac7b714e0211be88b61131201a6c97d
kanishh7 <109466541+kanishh7@users.noreply.github.com>
2024-03-24 05:10:03 -0400

    Update

--- HW3/medium_model.py
+++ HW3/medium_model.py
@@ -4,7 +4,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from transformers import BertModel, BertTokenizerFast, AdamW
 from torch.utils.data import Dataset, DataLoader
-from torch.optim.lr_scheduler import ExponentialLR
 from evaluate import load
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
